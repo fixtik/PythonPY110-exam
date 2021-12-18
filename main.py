6c26e9fa4dea84af739c5ab4113b2339b0abbd0f
Alex <alx@mail.ru>
2021-12-18 15:15:53 +0300

    add result

---
+++ main.py
@@ -0,0 +1,12 @@
+import generate_books
+import json
+
+O_FILE = 'result.txt'
+
+def main():
+    with open(O_FILE, 'w') as j_dump:
+        json.dump(generate_books.dit_creator(count=100),j_dump, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
